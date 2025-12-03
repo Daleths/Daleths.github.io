ec838128341e92e974639481545468723dc31969
Daleths <116331514+Daleths@users.noreply.github.com>
2025-12-03 14:05:36 +0700

    Update label

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Malware Analysis",
+	title: "Redteam Techiques",
 	subtitle: "",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
